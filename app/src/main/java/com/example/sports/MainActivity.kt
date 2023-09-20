80fcd9c6eebadf01a6f1e0362970f119ccc3cb4f
Kordeliia <lalvmun@hotmail.com>
2023-09-20 13:11:56 +0200

    Commit con la configuracion de RecyclerView

--- app/src/main/java/com/example/sports/MainActivity.kt
+++ app/src/main/java/com/example/sports/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.sports
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sports.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listAdapter: SportListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+    }
+    private fun setupRecyclerView(){
+        listAdapter = SportListAdapter(this)
+        binding.recyclerView.apply{
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = listAdapter
+        }
+    }
+
+    override fun onClick(sport: Sport) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/sports/SportListAdapter.kt
+++ app/src/main/java/com/example/sports/SportListAdapter.kt
@@ -1,19 +1,32 @@
 package com.example.sports
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.sports.databinding.ItemSportBinding
 
 class SportListAdapter(private val listener: OnClickListener) : ListAdapter<Sport, RecyclerView.ViewHolder>(SportDiffCallBack()){
+    private lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_sport, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val sport = getItem(position)
+        with(holder as ViewHolder){
+            binding.tvName.text = sport.name
+            Glide.with(context).load(sport.imgURL).into(binding.imgPhoto)
+        }
     }
     inner class ViewHolder (view: View) : RecyclerView.ViewHolder(view){
-
+        val binding = ItemSportBinding.bind(view)
     }
 
     class SportDiffCallBack: DiffUtil.ItemCallback<Sport>(){
